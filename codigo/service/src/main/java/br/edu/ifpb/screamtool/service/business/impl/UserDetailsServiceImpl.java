41ce4ac381469f9c03bf72d47b8b4cfa91e30e47
edsf <ednaldo_dilorenzo@hotmail.com>
2015-04-13 22:13:40 -0300

    Todos os arquivos de html.

--- codigo/service/src/main/java/br/edu/ifpb/screamtool/service/business/impl/UserDetailsServiceImpl.java
+++ codigo/service/src/main/java/br/edu/ifpb/screamtool/service/business/impl/UserDetailsServiceImpl.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Service;
  * @author edsf
  *
  */
-@Service("usuarioService")
+@Service("userDetailsService")
 public class UserDetailsServiceImpl implements UserDetailsService {
 
 	/*
