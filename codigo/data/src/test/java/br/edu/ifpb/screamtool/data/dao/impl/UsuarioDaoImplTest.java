cf598c25cde99b978d7c01741a5eb9701ebb5bb2
edsf <ednaldo_dilorenzo@hotmail.com>
2015-04-28 11:30:33 -0300

    Novos arquivos adicionados.

---
+++ codigo/data/src/test/java/br/edu/ifpb/screamtool/data/dao/impl/UsuarioDaoImplTest.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package br.edu.ifpb.screamtool.data.dao.impl;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author edsf
+ *
+ */
+public class UsuarioDaoImplTest {
+	
+	@Test
+	public void fuckfuck(){
+		Assert.assertTrue(true);
+	}
+
+}
